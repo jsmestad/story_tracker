d6b095e8ec5718cd59298230f332f0ffdc07f3ec
Justin Smestad <justin.smestad@gmail.com>
2015-07-16 14:49:36 -0600

    Add page containing the requests made by the User

--- spec/features/propose_story_spec.rb
+++ spec/features/propose_story_spec.rb
@@ -12,9 +12,12 @@ RSpec.describe "Propose New Story", type: :feature, vcr: {record: :new_episodes}
     fill_in 'story_the_ask', with: 'slack integration'
     fill_in 'story_reasoning', with: 'I can stop importing invoices manually'
 
-    click_on 'Propose Story'
+    click_on 'Submit Story'
 
     expect(current_path).to eql('/iterations')
+
+    click_on 'Your Requests'
+    expect(page).to have_css('table tbody tr')
     # expect(page).to have_text('slack integration')
   end
 end
--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Story do
   it { is_expected.to belong_to(:user) }
+
+  it { is_expected.to validate_presence_of(:user) }
 end
