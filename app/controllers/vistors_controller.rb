a2503578446e5f560490244ebb157af2dc0ddd27
Justin Smestad <justin.smestad@gmail.com>
2015-07-19 19:56:36 -0600

    finalize supported flows

--- app/controllers/vistors_controller.rb
+++ app/controllers/vistors_controller.rb
@@ -1,3 +1,11 @@
 class VistorsController < ApplicationController
+  before_filter :redirect_if_logged_in
+
+  def redirect_if_logged_in
+    if user_signed_in?
+      redirect_to iterations_path
+      return false
+    end
+  end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,33 @@
 module ApplicationHelper
+  def calculate_risk(estimate)
+    case estimate
+    when 0
+      'none'
+    when 1
+      'low'
+    when 2
+      'marginal'
+    when 3
+      'high'
+    else
+      'high'
+    end
+  end
+
+  def story_type_icon(kind)
+    case kind
+    when 'feature'
+      content_tag(:i, nil, class: 'fi-star', title: 'Story Type: Feature')
+    when 'bug'
+      content_tag(:i, nil, class: 'fi-first-aid', title: 'Story Type: Bug')
+    when 'chore'
+      content_tag(:i, nil, class: 'fi-widget', title: 'Story Type: Chore')
+    when 'release'
+      content_tag(:i, nil, class: 'fi-flag', title: 'Story Type: Milestone')
+    end
+  end
+
+  def requested_by_user?(story)
+    Story.exists?(external_ref: story.id)
+  end
 end
