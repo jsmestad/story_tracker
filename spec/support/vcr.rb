aae565a34fb1a55e116e322a318e805c081a048a
Justin Smestad <justin.smestad@gmail.com>
2015-07-25 16:22:14 -0600

    filter out sensitive information from VCR

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,5 +4,7 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :webmock
   c.configure_rspec_metadata!
+  c.filter_sensitive_data('<TRACKERAPITOKEN>') { ENV['PIVOTAL_TRACKER_API_TOKEN'] }
+  c.filter_sensitive_data('<TRACKERPROJECT>') { ENV['DEFAULT_TRACKER_PROJECT'] }
 end
 
