4487d22da592def538f4dfe8bf5d3121c376a624
Justin Smestad <justin.smestad@gmail.com>
2015-09-03 10:16:08 -0600

    redirect users after login

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -2,16 +2,17 @@ require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
   describe "daily_summary" do
-    let(:mail) { UserMailer.daily_summary }
+    let!(:user) { FactoryGirl.create(:user, :with_email, token: SecureRandom.uuid) }
+    let!(:stories) { FactoryGirl.create_list(:story, 3, :with_activity, user: user) }
+    let(:mail) { UserMailer.daily_summary(user) }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Daily summary")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.subject).to eq("Daily StoryTracker Summary for #{Date.today.strftime('%B %d %Y')}")
+      expect(mail.to).to eq([user.email_address])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.body.encoded).to match("here is an update on all of your submissions as of")
     end
   end
 
