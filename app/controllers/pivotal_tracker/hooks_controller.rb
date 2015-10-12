d8391f38eb3bf9c8670195d2bce08c282430c74e
Justin Smestad <justin.smestad@gmail.com>
2015-10-12 16:38:25 -0600

    catch all types on PT callbacks

--- app/controllers/pivotal_tracker/hooks_controller.rb
+++ app/controllers/pivotal_tracker/hooks_controller.rb
@@ -10,7 +10,7 @@ module PivotalTracker
     include ActionController::Head
     include ActionController::Instrumentation
 
-    EVENTS = %w[ story_create_activity story_move_activity story_update_activity story_delete_activity ].freeze
+    EVENTS = %w[ story_create_activity story_started_activity story_estimated_activity story_move_activity story_update_activity story_delete_activity ].freeze
 
     def callback
       if ENV['CALLBACK_TOKEN'] != params[:token]
