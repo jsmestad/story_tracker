c96843f41a7ea3f8f20ea1630ff3bdf4655d8218
Justin Smestad <justin.smestad@gmail.com>
2015-10-16 10:14:14 -0600

    fix follow/unfollow

--- app/assets/javascripts/revised/application.js
+++ app/assets/javascripts/revised/application.js
@@ -16,7 +16,7 @@ jQuery(document).ready(function() {
         method: 'POST',
         success: function(data) {
           $link.removeClass('follow').addClass('unfollow');
-          $link.removeClass('fa-eye').addClass('fa-eye-slash').html(' Unfollow');
+          $link.html('<span class="fa fa-eye-slash"/> Unfollow');
           $link.closest('.story').toggleClass('tracked');
         }
       });
@@ -27,7 +27,7 @@ jQuery(document).ready(function() {
         data: {'_method': 'delete'},
         success: function(data) {
           $link.removeClass('unfollow').addClass('follow');
-          $link.removeClass('fa-eye-slash').addClass('fa-eye').html(' Follow');
+          $link.html('<span class="fa fa-eye"/> Follow');
           $link.closest('.story').toggleClass('tracked');
         }
       });
@@ -66,7 +66,7 @@ jQuery(document).ready(function() {
   $('div.story[data-local="false"] a.external').remove();
 
   $('div.story[data-local="true"] a.external').on('click', function(event) {
-		event.preventDefault();
+    event.preventDefault();
     var $link = $(this);
     $.ajax({
         url: $link.data('url'),
@@ -85,74 +85,74 @@ jQuery(document).ready(function() {
       });
   });
 
-  	//close the lateral panel
-	$('.cd-panel').on('click', function(event){
-		if( $(event.target).is('.cd-panel') || $(event.target).is('.cd-panel-close') ) {
-			$('.cd-panel').removeClass('is-visible');
-			event.preventDefault();
-		}
-	});
+    //close the lateral panel
+  $('.cd-panel').on('click', function(event){
+    if( $(event.target).is('.cd-panel') || $(event.target).is('.cd-panel-close') ) {
+      $('.cd-panel').removeClass('is-visible');
+      event.preventDefault();
+    }
+  });
 
   // BEGIN - secondary nav
   //
-	var secondaryNav = $('.cd-secondary-nav');
-		// secondaryNavTopPosition = secondaryNav.offset().top,
-		// taglineOffesetTop = $('#cd-intro-tagline').offset().top + $('#cd-intro-tagline').height() + parseInt($('#cd-intro-tagline').css('paddingTop').replace('px', '')),
-		// contentSections = $('.cd-section');
+  var secondaryNav = $('.cd-secondary-nav');
+    // secondaryNavTopPosition = secondaryNav.offset().top,
+    // taglineOffesetTop = $('#cd-intro-tagline').offset().top + $('#cd-intro-tagline').height() + parseInt($('#cd-intro-tagline').css('paddingTop').replace('px', '')),
+    // contentSections = $('.cd-section');
 
-	// $(window).on('scroll', function(){
-		// //on desktop - assign a position fixed to logo and action button and move them outside the viewport
-		// ( $(window).scrollTop() > taglineOffesetTop ) ? $('#cd-logo, .cd-btn').addClass('is-hidden') : $('#cd-logo, .cd-btn').removeClass('is-hidden');
+  // $(window).on('scroll', function(){
+    // //on desktop - assign a position fixed to logo and action button and move them outside the viewport
+    // ( $(window).scrollTop() > taglineOffesetTop ) ? $('#cd-logo, .cd-btn').addClass('is-hidden') : $('#cd-logo, .cd-btn').removeClass('is-hidden');
 
-		// //on desktop - fix secondary navigation on scrolling
-		// if($(window).scrollTop() > secondaryNavTopPosition ) {
-			// //fix secondary navigation
-			// secondaryNav.addClass('is-fixed');
-			// //push the .cd-main-content giving it a top-margin
-			// $('.cd-main-content').addClass('has-top-margin');
-			// //on Firefox CSS transition/animation fails when parent element changes position attribute
-			// //so we to change secondary navigation childrens attributes after having changed its position value
-			// setTimeout(function() {
-							// secondaryNav.addClass('animate-children');
-							// $('#cd-logo').addClass('slide-in');
-				// $('.cd-btn').addClass('slide-in');
-					// }, 50);
-		// } else {
-			// secondaryNav.removeClass('is-fixed');
-			// $('.cd-main-content').removeClass('has-top-margin');
-			// setTimeout(function() {
-							// secondaryNav.removeClass('animate-children');
-							// $('#cd-logo').removeClass('slide-in');
-				// $('.cd-btn').removeClass('slide-in');
-					// }, 50);
-		// }
+    // //on desktop - fix secondary navigation on scrolling
+    // if($(window).scrollTop() > secondaryNavTopPosition ) {
+      // //fix secondary navigation
+      // secondaryNav.addClass('is-fixed');
+      // //push the .cd-main-content giving it a top-margin
+      // $('.cd-main-content').addClass('has-top-margin');
+      // //on Firefox CSS transition/animation fails when parent element changes position attribute
+      // //so we to change secondary navigation childrens attributes after having changed its position value
+      // setTimeout(function() {
+              // secondaryNav.addClass('animate-children');
+              // $('#cd-logo').addClass('slide-in');
+        // $('.cd-btn').addClass('slide-in');
+          // }, 50);
+    // } else {
+      // secondaryNav.removeClass('is-fixed');
+      // $('.cd-main-content').removeClass('has-top-margin');
+      // setTimeout(function() {
+              // secondaryNav.removeClass('animate-children');
+              // $('#cd-logo').removeClass('slide-in');
+        // $('.cd-btn').removeClass('slide-in');
+          // }, 50);
+    // }
 
-		//on desktop - update the active link in the secondary fixed navigation
-		// updateSecondaryNavigation();
-	// });
+    //on desktop - update the active link in the secondary fixed navigation
+    // updateSecondaryNavigation();
+  // });
 
-	function updateSecondaryNavigation() {
-		contentSections.each(function(){
-			var actual = $(this),
-				actualHeight = actual.height() + parseInt(actual.css('paddingTop').replace('px', '')) + parseInt(actual.css('paddingBottom').replace('px', '')),
-				actualAnchor = secondaryNav.find('a[href="#'+actual.attr('id')+'"]');
-			if ( ( actual.offset().top - secondaryNav.height() <= $(window).scrollTop() ) && ( actual.offset().top +  actualHeight - secondaryNav.height() > $(window).scrollTop() ) ) {
-				actualAnchor.addClass('active');
-			}else {
-				actualAnchor.removeClass('active');
-			}
-		});
-	}
+  function updateSecondaryNavigation() {
+    contentSections.each(function(){
+      var actual = $(this),
+        actualHeight = actual.height() + parseInt(actual.css('paddingTop').replace('px', '')) + parseInt(actual.css('paddingBottom').replace('px', '')),
+        actualAnchor = secondaryNav.find('a[href="#'+actual.attr('id')+'"]');
+      if ( ( actual.offset().top - secondaryNav.height() <= $(window).scrollTop() ) && ( actual.offset().top +  actualHeight - secondaryNav.height() > $(window).scrollTop() ) ) {
+        actualAnchor.addClass('active');
+      }else {
+        actualAnchor.removeClass('active');
+      }
+    });
+  }
 
-	//on mobile - open/close secondary navigation clicking/tapping the .cd-secondary-nav-trigger
-	$('.cd-secondary-nav-trigger').on('click', function(event){
-		event.preventDefault();
-		$(this).toggleClass('menu-is-open');
-		secondaryNav.find('ul').toggleClass('is-visible');
-	});
+  //on mobile - open/close secondary navigation clicking/tapping the .cd-secondary-nav-trigger
+  $('.cd-secondary-nav-trigger').on('click', function(event){
+    event.preventDefault();
+    $(this).toggleClass('menu-is-open');
+    secondaryNav.find('ul').toggleClass('is-visible');
+  });
 
-	//smooth scrolling when clicking on the secondary navigation items
-	// secondarynav.find('ul a').on('click', function(event){
+  //smooth scrolling when clicking on the secondary navigation items
+  // secondarynav.find('ul a').on('click', function(event){
         // event.preventdefault();
         // var target = $(this.hash);
         // $('body,html').animate({
@@ -165,9 +165,9 @@ jQuery(document).ready(function() {
     // });
 
     //on mobile - open/close primary navigation clicking/tapping the menu icon
-	$('.cd-primary-nav').on('click', function(event){
-		if($(event.target).is('.cd-primary-nav')) $(this).children('ul').toggleClass('is-visible');
-	});
+  $('.cd-primary-nav').on('click', function(event){
+    if($(event.target).is('.cd-primary-nav')) $(this).children('ul').toggleClass('is-visible');
+  });
 
 
   //
